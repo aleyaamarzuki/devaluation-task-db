622ad95f6004f2f17c6aa4ad61f4e5723640e160
seowxft <62999342+seowxft@users.noreply.github.com>
2021-03-24 16:06:33 +0000

    update
    - save more questionnaire data

--- models/PsychQuiz.py
+++ models/PsychQuiz.py
@@ -18,15 +18,21 @@ class PsychQuiz(BaseObject, Model):
     PgFinish_OCIR    = Column(Text(length=10000))
     PgFinish_STAI_Y1  = Column(Text(length=10000))
     PgFinish_STAI_Y2  = Column(Text(length=10000))
+    PgFinish_BIS11    = Column(Text(length=10000))
+    PgFinish_SDS      = Column(Text(length=10000))
     PgRT_demo        = Column(Text(length=10000))
     PgRT_OCIR        = Column(Text(length=10000))
     PgRT_STAI_Y1     = Column(Text(length=10000))
     PgRT_STAI_Y2     = Column(Text(length=10000))
+    PgRT_BIS11       = Column(Text(length=10000))
+    PgRT_SDS         = Column(Text(length=10000))
     age              = Column(Text(length=10000))
     gender           = Column(Text(length=10000))
     OCIR             = Column(Text(length=10000))
     STAI_Y1          = Column(Text(length=10000))
     STAI_Y2          = Column(Text(length=10000))
+    BIS11            = Column(Text(length=10000))
+    SDS              = Column(Text(length=10000))
 
 
     def get_id(self):
@@ -56,8 +62,14 @@ class PsychQuiz(BaseObject, Model):
     def get_pg2_finish(self):
         return str(self.PgFinish_STAI_Y1)
 
-    def get_p3_finish(self):
+    def get_pg3_finish(self):
         return str(self.PgFinish_STAI_Y2)
+
+    def get_pg4_finish(self):
+        return str(self.PgFinish_BIS11)
+
+    def get_pg5_finish(self):
+        return str(self.PgFinish_SDS)
 
     def get_pg0_rt(self):
         return str(self.PgRT_demo)
@@ -70,6 +82,12 @@ class PsychQuiz(BaseObject, Model):
 
     def get_pg3_rt(self):
         return str(self.PgRT_STAI_Y2)
+
+    def get_pg4_rt(self):
+        return str(self.PgRT_BIS11)
+
+    def get_pg5_rt(self):
+        return str(self.PgRT_SDS)
 
     def get_age(self):
         return str(self.age)
@@ -85,6 +103,12 @@ class PsychQuiz(BaseObject, Model):
 
     def get_staiTwo(self):
         return str(self.STAI_Y2)
+
+    def get_bis(self):
+        return str(self.BIS11)
+
+    def get_sds(self):
+        return str(self.SDS)
 
     def errors(self):
         errors = super(PsychQuiz, self).errors()
